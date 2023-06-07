916a15cf925fcbe7168374755bb6c91ee9815c0f
David_drgn <72632974+David-drgn@users.noreply.github.com>
2023-06-06 22:18:08 -0300

    -- Continuação das telas --

--- Enfermagem/src/View/TelaPrincipal.java
+++ Enfermagem/src/View/TelaPrincipal.java
@@ -10,6 +10,7 @@ public class TelaPrincipal extends JFrame implements ActionListener {
 	private JPanel buttonPanel, medicoPanel, pacientePanel, atendentePanel;
 	private JButton buttons[];
 	private JLabel label[];
+	private JFrame c;
 	public TelaPrincipal( )
 	{
 		buttonPanel = new JPanel();
@@ -17,30 +18,30 @@ public class TelaPrincipal extends JFrame implements ActionListener {
 		buttonPanel.setSize(100,100);
 		
 		medicoPanel = new JPanel();
-		medicoPanel.setLayout(new GridLayout(2,4));
-		medicoPanel.setSize(100,100);
+		medicoPanel.setLayout(new GridLayout(4,2));
+		medicoPanel.setSize(200,200);
+		medicoPanel.setVisible(false);
 		
 		pacientePanel = new JPanel();
-		pacientePanel.setLayout(new GridLayout(2,4));
-		pacientePanel.setSize(100,100);
+		pacientePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+		pacientePanel.setSize(500,500);
 		
 		atendentePanel = new JPanel();
 		atendentePanel.setLayout(new GridLayout(1,2));
 		pacientePanel.setSize(100,100);
 		atendentePanel.setVisible(false);
 		
+		buttons = new JButton[ 10 ];
+		label = new JLabel[ 10 ];
+		
 		/* TELA PRINCIPAL */
 		
-		label = new JLabel[4];
-		
 		label[0] = new JLabel("--------------------", SwingConstants.CENTER);
-		label[1] = new JLabel("QUAL VOC  ", SwingConstants.RIGHT);
+		label[1] = new JLabel("QUAL VOCE ", SwingConstants.RIGHT);
 		label[2] = new JLabel("SERIA?", SwingConstants.LEFT);
 		label[3] = new JLabel("--------------------", SwingConstants.CENTER);
 		
-		buttons = new JButton[ 5 ];
-		
-		buttons[ 0 ] = new JButton( "Sou MÈdico" );
+		buttons[ 0 ] = new JButton( "Sou Medico" );
 		buttonPanel.add( buttons[ 0 ] );
 		
 		buttons[ 1 ] = new JButton( "Sou Atendente" );
@@ -62,13 +63,42 @@ public class TelaPrincipal extends JFrame implements ActionListener {
 	
 		/* TELA ATENDENTE */
 		
-		buttons[ 3 ] = new JButton( "Adicionar paciente" );
-		buttons[ 4 ] = new JButton( "Sair" );
+		buttons[ 4 ] = new JButton( "Adicionar paciente" );
+		buttons[ 5 ] = new JButton( "Sair" );
 		
-		atendentePanel.add(buttons[3]);
 		atendentePanel.add(buttons[4]);
+		atendentePanel.add(buttons[5]);
+		buttons[ 5 ].setBackground(Color.RED);
 		
-		JFrame c = new JFrame("Tela atendimento");
+		buttons[ 4 ].addActionListener(this);
+		buttons[ 5 ].addActionListener(this);
+		
+		/* TELA MEDICO */
+		
+		label[4] = new JLabel("--- Nome pr√≥ximo paciente: ");
+		label[5] = new JLabel("NOME PACIENTE");
+		label[6] = new JLabel("--- Senha: ");
+		label[7] = new JLabel("SENHA");
+		label[8] = new JLabel("--- PRIORIDADE: ");
+		label[9] = new JLabel("PRIORIDADE");
+		
+		buttons[ 6 ] = new JButton("VOLTAR");
+		buttons[ 6 ].setBackground(Color.RED);
+		buttons[ 7 ] = new JButton("ATENDER PACIENTE");
+		
+		buttons[6].addActionListener(this);
+		buttons[7].addActionListener(this);
+		
+		medicoPanel.add(label[4]); medicoPanel.add(label[5]);
+		medicoPanel.add(label[6]); medicoPanel.add(label[7]);
+		medicoPanel.add(label[8]); medicoPanel.add(label[9]);
+		medicoPanel.add(buttons[ 6 ]); medicoPanel.add(buttons[ 7 ]);
+		
+		/* TELA PACIENTE */
+		
+		
+		
+		c = new JFrame("Tela atendimento");
 		c.setSize(600,100);
 		
 		c.getContentPane().setLayout(new FlowLayout(FlowLayout.CENTER));
@@ -76,6 +106,8 @@ public class TelaPrincipal extends JFrame implements ActionListener {
 		c.getContentPane().add(buttonPanel);
 		
 		c.getContentPane().add(atendentePanel);
+		
+		c.getContentPane().add(medicoPanel);
 		
 		c.setVisible(true);
 	}
@@ -88,19 +120,43 @@ public class TelaPrincipal extends JFrame implements ActionListener {
 					{ System.exit( 0 ); } } );
 	}
 	public void actionPerformed(ActionEvent e) {
+		
+		/* BTN MEDICO */
 		if(e.getSource() == buttons[0]) {
 			buttonPanel.setVisible(false);
-			buttonPanel.setPreferredSize(new Dimension(0,0));
-			atendentePanel.setVisible(true);
+			buttonPanel.setSize(0,0);
+			medicoPanel.setVisible(true);
+			c.setSize(400,170);
 		}
+		
+		/* BTN ATENDENTE */
 		if(e.getSource() == buttons[1]) {
-			
+			buttonPanel.setVisible(false);
+			buttonPanel.setSize(0,0);
+			atendentePanel.setVisible(true);
+			c.setSize(300,80);
 		}
+		
+		/* BTN PACIENTE */
 		if(e.getSource() == buttons[2]) {
-			
+			buttonPanel.setVisible(false);
+			buttonPanel.setSize(0,0);
+			pacientePanel.setVisible(true);
+			c.setSize(500,500);
 		}
+		
+		/* BTN SAIR APLICACAO */
 		if(e.getSource() == buttons[3]) {
 			System.exit( 0 );
+		}
+		
+		/* BTNs VOLTAR INICIAL */
+		if(e.getSource() == buttons[6] || e.getSource() == buttons[5]) {
+			buttonPanel.setVisible(true);
+			buttonPanel.setSize(100,100);
+			c.setSize(600,100);
+			atendentePanel.setVisible(false);
+			medicoPanel.setVisible(false);
 		}
 	}
 }
